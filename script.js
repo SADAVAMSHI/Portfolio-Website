0587bf813b438a28affd6c5831dccd24464003bf
SADA VAMSHI .M <sadavamshimancholla@gmail.com>
2025-01-13 15:34:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const typed = new Typed('.typed', {
+    strings: ["Web Developer" , "Frontend developer" , "Designer"],
+    typeSpeed: 40,
+    backSpeed: 30,
+    loop: true,
+  });
+});
